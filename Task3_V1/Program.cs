edacd16f1929b55bc56099d3c1a70b3fbd2cac2e
Grig <chiefy@mail.ru>
2022-09-20 23:04:55 +0200

    Arranged repository

--- Task3_V1/Program.cs
+++ Task3_V1/Program.cs
@@ -2,20 +2,20 @@
 // Найдите элементы, у которых оба индекса чётные, и 
 // замените эти элементы на их квадраты.
 
+Console.Clear();
+
 Console.Write("Введите количество строк массива: ");
 int rows = int.Parse(Console.ReadLine());
 
 Console.Write("Введите количество столбцов массива: ");
 int columns = int.Parse(Console.ReadLine());
 
-int[,] array = GetArray(rows, columns, 2, 12);
-PrintArray(array);
-Console.WriteLine();
-GetPow(array);
+int[,] array = GetArray(rows, columns, 0, 9);
 PrintArray(array);
 
-//int arr = array.Length; - покажет сколько всего элементов в массиве
-//Console.WriteLine(arr);
+Console.WriteLine();
+int[,] processedArray = ProcessArray(array);
+PrintArray(processedArray);
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -29,6 +29,23 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     }
     return result;
 }
+int[,] ProcessArray(int[,] intArray)
+{
+    int m = intArray.GetLength(0);
+    int n = intArray.GetLength(1);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                intArray[i, j] = intArray[i, j] * intArray[i, j];
+            }
+        }
+    }
+    return intArray;
+}
 
 void PrintArray(int[,] inArray)
 {
@@ -39,16 +56,5 @@ void PrintArray(int[,] inArray)
             Console.Write($"{inArray[i, j]} ");
         }
         Console.WriteLine();
-    }
-}
-void GetPow(int[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            if (i % 2 == 0 && j % 2 == 0)
-                inArray[i, j] = inArray[i, j] * inArray[i, j];
-        }
     }
 }
---
+++ Task4_V1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д. 
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 9);
+PrintArray(array);
+
+Console.WriteLine();
+
+int MatrixSum(int[,] intArray)
+{
+    int m = intArray.GetLength(0);
+    int n = intArray.GetLength(1);
+    int len = 0;
+    int sum = 0;
+    if (m > n) len = n;
+    else len = m;
+
+    for (int i = 0; i < len; i++)
+    {
+        sum = sum + intArray[i, i];
+    }
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
