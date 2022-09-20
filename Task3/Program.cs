51a10e6699ea3cf3ad9c06161b349aa14f51d41a
Grig <chiefy@mail.ru>
2022-09-20 21:09:37 +0200

    Add Seminar 7 working Tasks

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, и 
+// замените эти элементы на их квадраты.
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 2, 12);
+PrintArray(array);
+Console.WriteLine();
+GetPow(array);
+PrintArray(array);
+
+//int arr = array.Length; - покажет сколько всего элементов в массиве
+//Console.WriteLine(arr);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void GetPow(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            inArray[i, j] = inArray[i, j] * inArray[i, j];
+        }
+    }
+}
+
+
+
+//or
+
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = GetArray(rows, columns, 0, 9);
+// PrintArray(array);
+
+// Console.WriteLine();
+// int[,] processedArray = ProcessArray(array);
+// PrintArray(processedArray);
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+// int[,] ProcessArray(int[,] intArray)
+// {
+//     int m = intArray.GetLength(0);
+//     int n = intArray.GetLength(1);
+
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             if (i%2==0 && j%2==0)
+//             {
+//                 intArray[i, j] = intArray[i, j]*intArray[i, j];
+//             }
+//         }
+//     }
+//     return intArray;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int minValue = 0;
+int maxValue = 9;
+int[,] array = GetArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine($" Сумма чисел по диагонали = {GetMultiply(array)}");
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetMultiply(int[,] inArray)
+{
+    int Sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j) Sum += inArray[i, j];
+        }
+    }
+    return Sum;
+}
+
+// or
+
+// int GetSumDiagonalArray(int[,] arrayNew)
+// {
+//     int m = arrayNew.GetLength(0);
+//     int n = arrayNew.GetLength(1);
+//     int sum = 0;
+//     for(int i = 0; i<m; i++)
+//     {
+//         for(int j = 0; j<n; j++)
+//         {
+//             if(i == j)
+//             sum += arrayNew[i,j];
+//         }
+//     }
+//     return sum;
+// }
+
+//or
+
+// Console.Clear();
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = GetArray(rows, columns, 0, 9);
+// PrintArray(array);
+
+// Console.WriteLine();
+// int result = 
+// int MatrixSum(int[,] intArray)
+// {
+//     int m = intArray.GetLength(0);
+//     int n = intArray.GetLength(1);
+//     int len = 0;
+//     int sum = 0;
+//     if (m > n) len = n;
+//     else len = m;
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         sum = sum + intArray[i, i];
+//     }
+//     return sum;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
