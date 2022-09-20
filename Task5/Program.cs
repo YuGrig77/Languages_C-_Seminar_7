836e5a242505503f4a626a571ba193e380e60c60
Grig <chiefy@mail.ru>
2022-09-20 22:57:38 +0200

    Saved Task 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д. 
+
