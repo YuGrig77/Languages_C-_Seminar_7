a52c5faf6c86925ea990153611bd4a4016f9fcd8
Grig <chiefy@mail.ru>
2022-09-21 13:31:44 +0200

    Add Home Work Task 47 and alternative method

---
+++ HomeWork_Task47/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Pls enter number of rows: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Pls enter number of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+int minValue = -100;
+int maxValue = 120;
+
+double[,] array = FillArray(rows, columns, minValue, maxValue);
+PrintArray(array);
+
+double[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] working = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            working[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1)) / 10;
+        }
+    }
+    return working;
+}
+
+void PrintArray(double[,] inarray)
+{
+    for (int i = 0; i < inarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inarray.GetLength(1); j++)
+        {
+            Console.Write($"{inarray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ HomeWork_Task47_V1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Pls enter number of rows: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Pls enter number of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = FillArray(rows, columns);
+PrintArray(array);
+
+double[,] FillArray(int m, int n)
+{
+    double[,] working = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            working[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return working;
+}
+
+void PrintArray(double[,] inarray)
+{
+    for (int i = 0; i < inarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inarray.GetLength(1); j++)
+        {
+            Console.Write($" {Math.Round((inarray[i, j]), 1)} ");
+        }
+        Console.WriteLine();
+    }
+}
